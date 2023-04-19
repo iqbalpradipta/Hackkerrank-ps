b8c2bd2d4157cd6c210a53a02eb75bf9da7c3cde
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-04-19 22:56:21 +0700

    ps

---
+++ DivisionBill.go
@@ -0,0 +1,12 @@
+package main
+
+func bonAppetit(bill []int32, k int32, b int32) {
+    // Write your code here
+	var n int32 = len(bill)
+	var i int32
+	var sum int32
+	for i = 0; i <= n; i++ {
+		sum += bill[i]
+		if 
+	}
+}

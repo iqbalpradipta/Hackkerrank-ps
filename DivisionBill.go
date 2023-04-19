49b08eb1c64012f85dff42cb550750dfdb4a0990
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-04-19 23:16:42 +0700

    ps

--- DivisionBill.go
+++ DivisionBill.go
@@ -1,12 +1,28 @@
 package main
 
-func bonAppetit(bill []int32, k int32, b int32) {
+import "fmt"
+
+func bonAppetit(bill []int32, k int32, b int32){
     // Write your code here
-	var n int32 = len(bill)
-	var i int32
 	var sum int32
-	for i = 0; i <= n; i++ {
+	var b2 int32
+	var sumsum int32
+	for i := 0; i < len(bill); i++ {
 		sum += bill[i]
-		if 
-	}
+		if int32(i) == k {
+			sum -= bill[k]
+			b2 = sum % 2
+			sumsum = b - b2
+		} else if k == b {
+			fmt.Println("Bon Appetit")
+		}
+	} 
+	fmt.Println(sumsum)
+}
+
+func main()  {
+	var bill = []int32{3, 10, 2, 9}
+	var k int32 = 1
+	var b int32 = 12
+	bonAppetit(bill, k, b)
 }

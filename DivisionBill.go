6ab3c1980f0952f6976c49f706b5493fe3eb7012
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-04-20 00:59:31 +0700

    ps

--- DivisionBill.go
+++ DivisionBill.go
@@ -4,25 +4,29 @@ import "fmt"
 
 func bonAppetit(bill []int32, k int32, b int32){
     // Write your code here
-	var sum int32
-	var b2 int32
-	var sumsum int32
-	for i := 0; i < len(bill); i++ {
-		sum += bill[i]
-		if int32(i) == k {
-			sum -= bill[k]
-			b2 = sum % 2
-			sumsum = b - b2
-		} else if k == b {
-			fmt.Println("Bon Appetit")
-		}
-	} 
-	fmt.Println(sumsum)
+    var sum int32
+    var newBill int32 = k
+    bill = append(bill[:newBill], bill[newBill + 1:]...)
+    var b2 int32
+    var res int32
+    var text string = "Bon Appetit"
+    for i := 0; i < len(bill); i++ {
+        sum += bill[i] 
+        b2 = sum / 2
+		res = b - b2
+        if  b2 == b{
+            fmt.Println(text)
+			break
+        }else if int(bill[i]) > len(bill) {
+            fmt.Println(res)
+			break
+        }
+    }
 }
 
 func main()  {
-	var bill = []int32{3, 10, 2, 9}
-	var k int32 = 1
-	var b int32 = 12
+	var bill = []int32{72, 53, 60, 66, 90, 62, 12, 31, 36, 94}
+	var k int32 = 6
+	var b int32 = 288
 	bonAppetit(bill, k, b)
 }

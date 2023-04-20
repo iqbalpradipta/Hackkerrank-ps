232626a9d4d637487bfb19a83d2d92fad2c7293e
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-04-20 22:07:33 +0700

    ps

---
+++ PlusMinus.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func plusMinus(arr []int32) {
+    // Write your code here
+	var resultPositive float64 = 0.0
+	var resultNegative float64 = 0.0
+	var resultZero float64 = 0.0
+	var n float64 = float64(len(arr))
+	
+	for v := range arr {
+		if arr[v] > 0 {
+			resultPositive++
+			fmt.Printf("%.6f\n", resultPositive/n)
+		} else if arr[v] < 0 {
+			resultNegative++
+			fmt.Printf("%.6f\n", resultNegative/n)
+		} else {
+			resultZero++
+			fmt.Printf("%.6f\n", resultZero/n)
+		}
+	}
+}
+
+func main()  {
+	var arr []int32 = []int32{-4, 3, -9, 0, 4, 1}
+	// var n int32 = int32(len(arr))
+
+	plusMinus(arr)
+}

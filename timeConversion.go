2d9375aabafd5e7133f9cc87f9bda9dfcdb92415
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-04-20 23:51:17 +0700

    ps

---
+++ timeConversion.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func timeConversion(s string) string {
+    // Write your code here
+	t, _ := time.Parse("03:04:05PM", s)
+	return t.Format("15:04:05")
+
+}
+
+func main()  {
+	s := "07:05:45PM"
+	fmt.Println(timeConversion(s))
+}

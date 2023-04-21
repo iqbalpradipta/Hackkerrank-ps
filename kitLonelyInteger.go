475087f91426b430a8d9f825d89828c852b9d9b1
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-04-21 20:46:57 +0700

    ps

---
+++ kitLonelyInteger.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+
+func lonelyinteger(a []int32) int32 {
+    // Write your code here
+	var sum int32
+	for i := 0; i < len(a); i++ {
+		sum ^= a[i]
+	}
+	return sum
+}
+
+func main()  {
+	a := []int32{0,0,2,4,4,1,1}
+	fmt.Println(lonelyinteger(a))
+}

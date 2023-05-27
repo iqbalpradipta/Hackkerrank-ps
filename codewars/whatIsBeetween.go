6f7dd45088b3df16038805d4b5ee7f0f2f755b91
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-05-27 19:27:37 +0700

    codewars

--- codewars/whatIsBeetween.go
+++ codewars/whatIsBeetween.go
@@ -1,2 +1,21 @@
 package main
 
+import "fmt"
+
+func Between(a, b int) []int {
+	// your code here
+	var sum int
+	var sumAppend []int
+	for i := a; i <= b; i++ {
+		sum = i
+
+		sumAppend = append(sumAppend, sum)
+	}
+	return sumAppend
+}
+
+func main()  {
+	a := 1
+	b := 4
+	fmt.Println(Between(a,b))	
+}

1b2d60fe8efd9086ba7f2c0d2c940c0ac0eaa90e
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-06-17 08:45:02 +0700

    ps complete

--- chocolate.go
+++ chocolate.go
@@ -15,20 +15,15 @@ func chocolateFeast(n int32, c int32, m int32) int32 {
 	var exchange int32
 	chocolate = n/c
 	exchange = chocolate
-	var result int32
-
-	if chocolate == m {
-		result = exchange + 1
-	} else if chocolate < m {
-		result = exchange
-	} else if chocolate > m {
-		result = exchange * m - 1
+	for exchange >= m {
+		exchange = exchange - m + 1
+		chocolate++
 	}
-	return result
+	return chocolate
 }
 
 func main()  {
-	n := 10
+	n := 6
 	c := 2
 	m := 2
 	fmt.Println(chocolateFeast(int32(n),int32(c),int32(m)))
